f293205e03b0367c432c9a7d1b8b5bb67e34c82f
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-08-22 16:12:25 -0700

    additions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Navbar,Nav} from "react-bootstrap";
+import {Navbar,Nav,NavDropdown} from "react-bootstrap";
 import {Link,BrowserRouter,Switch,Route} from "react-router-dom";
 import about from "./Routes/About";
 import home from "./Routes/Home";
 import PW from "./Routes/Products";
 import WD from "./Routes/Products";
+import './App.css'
 
 
 
@@ -13,11 +14,14 @@ function App() {
   return (
       <>
           <BrowserRouter>
-              <Navbar bg="dark" expand="lg" variant="dark" sticky={"top"}>
+              <Navbar bg="dark" expand="sm" variant="dark" className={"navbarr"}>
           <Navbar.Brand ><img src={require('./Assets /360Logo.png')} style={styles.logo}/><Link to={"/Home"}/></Navbar.Brand>
           <Nav className="mr-auto">
           <Nav.Link  eventKey={"1"} href={'/Home'} style={styles.NavLinks}>Home</Nav.Link>
-              <Nav.Link href="/Products"  style={styles.NavLinks}>Products</Nav.Link>
+              <NavDropdown title={"Product"} id={"basic-nav-dropdown"} style={styles.NavLinks}>
+                  <NavDropdown.Item href={"WD"}><Link to={"/WD"}>Wash & Detail</Link></NavDropdown.Item>
+                  <NavDropdown.Item href={"PW"}><Link to={"/PW"}>Polish</Link></NavDropdown.Item>
+              </NavDropdown>
               <Nav.Link href="/About" eventKey={"3"} style={styles.NavLinks}><Link to={"/Home"}/>About</Nav.Link>
             </Nav>
 
