366c4e6d939eab05814400bfd87f87ab50f631cb
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-08-22 09:23:11 -0700

    react-boostrap addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar,FormControl,Button,Nav,Form,NavDropdown} from "react-bootstrap";
+
 
 function App() {
   return (
 <div>oscar</div>
-
   );
 }
 
