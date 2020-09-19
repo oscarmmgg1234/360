8d676bb0fa58f1f803ea9081fe6b793b0b2f629f
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-09-18 21:34:06 -0700

    homeStatus

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import '../RoutesCSS/Home.css'
 
 export default class home extends React.Component{
 render() {
     return(
-<div>oscar</div>
+    <>
+        <div className={"topHeader"}>
+        topheader
+        </div>
+        <div className={"content"}>content
+        </div>
+        <div className={"contact"}>
+        contact
+        </div>
+    </>
 
     );
 }
